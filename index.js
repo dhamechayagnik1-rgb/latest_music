0130dcfb919b1b8978679ab5a2f5b36e35614cc8
dhamechayagnik1 <dhamechayagnik1@gmail.com>
2026-03-01 11:59:23 +0530

    my second commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ res.sendFile(path.join(path.resolve(),"MUSIC-WEBISITE" , "src",
 "view", "index.html"))
 })
 
-
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
 console.log('Server running on port  ${PORT}');
 });
